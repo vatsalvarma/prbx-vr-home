a6cca8badfe77ee5fe98a912f2cb66c4f21f2b28
Vatsal varma <144918256+vatsalvarma@users.noreply.github.com>
2026-05-09 12:06:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const images = [ "https://images.unsplash.com/photo-1507525428034-b723cf961d3ev", 
+                "https://images.unsplash.com/photo-1493244040629-496f6d136cc3v", 
+                "https://images.unsplash.com/photo-1501785888041-af3ef285b470v",
+                "https://images.unsplash.com/photo-1491553895911-0055eca6402dv",
+                "https://images.unsplash.com/photo-1500530855697-b586d89ba3eev", 
+                "https://images.unsplash.com/photo-1506744038136-46273834b3fbv" ];
